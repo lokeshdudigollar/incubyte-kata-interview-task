22800c880c79c0303a2763bf2aa33451aa983fd3
lokeshdudigollar <lokesh.dudigollar@gmail.com>
2026-03-17 02:00:12 +0530

    feat: implemented employee repository

---
+++ app/repositories/employee_repository.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Session
+from app.models import Employee
+
+
+class EmployeeRepository:
+
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create_employee(self, full_name, job_title, country, salary):
+        employee = Employee(
+            full_name=full_name,
+            job_title=job_title,
+            country=country,
+            salary=salary
+        )
+
+        self.db.add(employee)
+        self.db.commit()
+        self.db.refresh(employee)
+
+        return employee
+
+    def get_employee(self, employee_id: int):
+        return self.db.query(Employee).filter(Employee.id == employee_id).first()
